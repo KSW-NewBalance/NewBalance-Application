c204c5a1fa4462e1f3749761056d1f7ea00c2c3d
Minju Kim <mindori1019@gmail.com>
2023-02-13 15:54:13 -0500

    Feature/running: Created bottom sheet and running information container

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,13 @@
 const String google_api_key = 'AIzaSyA5zxSbB16eMscJpvC1jIdLU_eUJJvONo0';
 const String primaryColor = '0xff5BD4EA';
 const String secondaryColor = '0xFFCD37';
+
+
+/**
+ * Main Page
+ */
+const String start = 'Start';
+const String runningInformation = 'Running Information';
+const String distance = 'Distance';
+const String totalTime = 'Total Time';
+const String averagePage = 'Average Pace';
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
-import 'package:newbalance_flutter/constants.dart';
+import 'package:newbalance_flutter/constants.dart' as constants;
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key, required this.title});
@@ -16,7 +16,6 @@ class _MainPageState extends State<MainPage> {
   late GoogleMapController _controller;
 
   Location _location = Location();
-
   LatLng _currentLatLng =
       LatLng(40.42599720832946, -86.90980084240438); // K-SW location
 
@@ -29,6 +28,62 @@ class _MainPageState extends State<MainPage> {
         _currentLatLng = LatLng(l.latitude!, l.longitude!);
       });
     });
+  }
+
+  Container _showBottomSheet() {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 332,
+            color: Colors.grey[200],
+            padding: EdgeInsets.symmetric(vertical: 35, horizontal: 30),
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  constants.runningInformation,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Colors.white,
+                  ),
+                  margin: EdgeInsets.fromLTRB(0, 17, 0, 21),
+                  padding: EdgeInsets.symmetric(vertical: 40, horizontal: 28),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: const [
+                          Text('0.00', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),),
+                          Text(constants.distance, style: TextStyle(fontSize: 12),),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          Text('00:00', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),),
+                          Text(constants.totalTime, style: TextStyle(fontSize: 12),),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          Text('_\'__\"', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),),
+                          Text(constants.averagePage, style: TextStyle(fontSize: 12),),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        });
+    return Container();
   }
 
   Stack _buildView() {
@@ -48,10 +103,13 @@ class _MainPageState extends State<MainPage> {
           margin: EdgeInsets.fromLTRB(20.0, 0, 20.0, 42.0),
           alignment: Alignment.bottomCenter,
           child: ElevatedButton(
-            onPressed: () {},
-            child: Text('Start'),
+            onPressed: _showBottomSheet,
+            child: Text(constants.start),
             style: ElevatedButton.styleFrom(
-              textStyle: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w700, color: Colors.white),
+              textStyle: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white),
               minimumSize: Size.fromHeight(64),
               shadowColor: Colors.grey,
             ),
